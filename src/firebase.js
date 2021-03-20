49af7e9b80ab0bcd519c2fa7b8fb3e1e87befc73
sonalan <sinan@onalan.net>
2021-03-20 00:13:09 -0700

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "slackt-db.firebaseapp.com",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1::web:"
+  }; 
