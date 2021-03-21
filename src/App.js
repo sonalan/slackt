3e9c5b7823843fc23d66fa1724cf4307076691f8
sonalan <sinan@onalan.net>
2021-03-21 14:12:57 -0700

    defined SidebarHeader, SidebarInfo properties

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import styled from 'styled-components';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
 
 export default function Sidebar() {
     return (
@@ -11,14 +12,57 @@ export default function Sidebar() {
                     <h2>Slackt Chat</h2>
                     <h3>
                         <FiberManualRecordIcon />
+                        Sinan Onalan
                     </h3>
                 </SidebarInfo>
+                <CreateIcon/>
             </SidebarHeader>
         </SidebarContainer>
     )
 }
 
 
-const SidebarContainer = styled.div``
-const SidebarHeader = styled.div``
-const SidebarInfo = styled.div``
+const SidebarContainer = styled.div`
+    background-color: var(--slack-color);
+    color: #ffffff;
+    flex: 0.3; 
+    border:1px solid #49274b;
+    margin-top:60px;
+`
+const SidebarHeader = styled.div`
+    display:flex;
+    border-bottom:1px solid #49274b;
+    padding:13px;
+    padding-bottom:10px;
+    
+    > .MuiSvgIcon-root{
+        padding: 8px;
+        color: #49274b;
+        font-size: 18px;
+        background-color: #ffffff;
+        border-radius:999px;
+    }
+
+`
+const SidebarInfo = styled.div`
+    flex:1;
+    > h2{
+        font-size:15px;
+        font-weight:900;
+        margin-bottom:5px;
+    }
+
+    > h3{
+        display:flex;
+        font-size:13px;
+        font-weight:400;
+        align-items:center;
+    }
+
+    > h3 >  .MuiSvgIcon-root{
+        font-size: 14px;
+        margin-top:1px;
+        margin-right:2px;
+        color:green;
+    }
+`
