2d7573d843986d1288de21c90e2912b3bef89719
sonalan <sinan@onalan.net>
2021-03-21 15:54:48 -0700

    Added firebase and set db, auth, auth.provider

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "slackt-db.firebaseapp.com",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "1::web:"
+    apiKey: "your_api_key",
+    authDomain: "your_auth_domain",
+    projectId: "your_project_id",
+    storageBucket: "your_storage_bucket",
+    messagingSenderId: "messaging_sender_id",
+    appId: "your_app_Id"
   }; 
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db= firebaseApp.firestore(); 
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
