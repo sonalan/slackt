efefa1166d9a411842922df1a9aa080ede3b7052
sonalan <sinan@onalan.net>
2021-03-22 18:08:02 -0700

    getting messages from firebase with roomId

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,19 +4,37 @@ import styled from 'styled-components';
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined"
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined"
 import ChatInput from '../ChatInput/ChatInput';
+import Message from '../Message/Message';
 
 import { useSelector } from 'react-redux';
 import { selectRoomId } from '../../features/appSlice';
+import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
+
+import { db } from '../../firebase';
 
 
 export default function Chat() {
     const roomId = useSelector(selectRoomId);
+    const [roomDetails] = useDocument(
+        roomId && db.collection('rooms').doc(roomId)
+    );
+
+    const [roomMessages] = useCollection(
+        roomId && 
+            db
+            .collection('rooms')
+            .doc(roomId)
+            .collection('messages')
+            .orderBy('timeStamp','asc')
+    ); 
+    //console.log(roomDetails?.data());
+    const channelName=roomDetails?.data()?.name;
     return (
         <ChatContainer>
             <>
             <ChatHeader>
                 <HeaderLeft>
-                    <h4>#Room Name</h4>
+                    <h4>#{channelName}</h4>
                     <StarBorderOutlinedIcon/>
                 </HeaderLeft>
                 <HeaderRight>
@@ -26,8 +44,22 @@ export default function Chat() {
                     </p>
                 </HeaderRight>
             </ChatHeader>
-            <ChatMessages>Mesajlar pambık gibi mesajlar</ChatMessages>
-            <ChatInput channelId={roomId}/>
+            <ChatMessages>
+                {roomMessages?.docs.map(doc=>{
+                    const { message, userName, userPic,timeStamp, isDeleted} = doc.data();
+                    return (
+                        <Message 
+                            key={doc.id}
+                            message={message} 
+                            userName={userName}
+                            userPic={userPic}
+                            timeStamp={timeStamp} 
+                            isDeleted={isDeleted}
+                        />
+                    )
+                })}
+            </ChatMessages>
+            <ChatInput channelId={roomId} channelName={channelName}/>
             
             </>
         </ChatContainer>
